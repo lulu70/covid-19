d25846aaef7e7caa9288b7d6e09829c8259dafd4
lulu70 <liorco70@yahoo.com>
2020-03-23 21:28:19 +0100

    design change:

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,13 @@
 import React from "react"
-
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404: Not found" />
     <div
       style={{
+        height: "100vh",
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
@@ -26,7 +25,7 @@ const NotFoundPage = () => (
         GO BACK
       </Link>
     </div>
-  </Layout>
+  </>
 )
 
 export default NotFoundPage
